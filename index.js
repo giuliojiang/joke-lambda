9c7278fdd963cfbaca46ebe87c8f86529c5e02db
giuliojiang <giuliojiang@gmail.com>
2018-02-20 13:25:13 +0000

    Wire in POST request and response

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 var path = require("path");
+var bodyParser = require("body-parser");
 
 var EXPRESS_PORT = 23000;
+
+// Configure bodyparser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
@@ -11,6 +16,13 @@ app.get("/", (req, res) => {
 app.get("/chat/:msg", (req, res) => {
     var msg = req.params.msg;
     res.send("Got your message. Message is " + msg);
+});
+
+app.post("/pg", (req, res) => {
+    console.info("POST: " + JSON.stringify(req.body));
+    res.send(JSON.stringify({
+        _t: "Test response"
+    }));
 });
 
 app.listen(EXPRESS_PORT, () => {
